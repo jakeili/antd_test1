d66a7a3c37ac61c5be48d047057dafd25a79b9e7
jakeili <895301470@qq.com>
2020-12-09 00:19:28 +0800

    login 静态完成

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,9 +1,13 @@
 import {Component} from 'react'
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 import './login.less'
 import logo from '../../assets/images/logo.png'
 export default class Login extends Component{
-
+  onFinish = values => {
+    console.log('Received values of form: ', values);
+  };
   render(){
     return (
       <div className= "login">
@@ -13,9 +17,44 @@ export default class Login extends Component{
         </header>
         <section className="content">
           <h2>用户登录</h2>
-          <div className="form-box">
-            
-          </div>
+          <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{ remember: true }}
+            onFinish={this.onFinish}
+          >
+            <Form.Item
+              name="username"
+              rules={[{ required: true, message: 'Please input your Username!' }]}
+            >
+              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[{ required: true, message: 'Please input your Password!' }]}
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <a className="login-form-forgot" href="">
+                Forgot password
+              </a>
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                Log in
+              </Button>
+            </Form.Item>
+          </Form>
         </section>
       </div>
     )
